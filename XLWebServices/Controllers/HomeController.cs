709be7ee63839c089237250f347888c41ac722f6
goaaats <16760685+goaaats@users.noreply.github.com>
2022-01-28 18:14:56 +0100

    index page

---
+++ XLWebServices/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XLWebServices.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet("/")]
+    public async Task<IActionResult> Index()
+    {
+        return Content("<h1>XL Web Services</h1>" +
+                       "This server provides updates for XIVLauncher and the plugin listing for Dalamud.<br>" +
+                       "<a href=\"https://goatcorp.github.io/faq/xl_troubleshooting#q-are-xivlauncher-dalamud-and-dalamud-plugins-safe-to-use\">Read more here.</a>", "text/html");
+    }
+}
