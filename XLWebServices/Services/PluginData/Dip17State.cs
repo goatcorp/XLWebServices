44b28193393d9c764394bc3c85ce233964676abb
goat <goatsdev@protonmail.com>
2024-10-05 15:33:55 +0200

    update state schema

--- XLWebServices/Services/PluginData/Dip17State.cs
+++ XLWebServices/Services/PluginData/Dip17State.cs
@@ -5,6 +5,7 @@ public class Dip17State
     public Dip17State()
     {
         this.Channels = new Dictionary<string, Channel>();
+        this.ReviewedNeeds = new List<Need>();
     }
 
     public class Channel
@@ -13,29 +14,63 @@ public class Dip17State
         {
             this.Plugins = new Dictionary<string, PluginState>();
         }
-        
+
         public class PluginState
         {
             public PluginState()
             {
                 this.Changelogs = new Dictionary<string, PluginChangelog>();
             }
-            
+
             public string BuiltCommit { get; set; }
             public DateTime TimeBuilt { get; set; }
             public string EffectiveVersion { get; set; }
-            
+            public string? MinimumVersion { get; set; }
+
             public Dictionary<string, PluginChangelog> Changelogs { get; set; }
 
+            // This should really be "version", but we're stuck with "changelog" for now
             public class PluginChangelog
             {
                 public DateTime TimeReleased { get; set; }
-                public string Changelog { get; set; }
+                public string? Changelog { get; set; }
+
+                public class UsedNeed
+                {
+                    public string Key { get; set; }
+                    public string Version { get; set; }
+                }
+
+                public List<UsedNeed>? UsedNeeds { get; set; }
+
+                public string Reviewer { get; set; }
             }
         }
-        
+
         public IDictionary<string, PluginState> Plugins { get; set; }
     }
-    
+
     public IDictionary<string, Channel> Channels { get; set; }
+
+    public class Need
+    {
+        public enum NeedType
+        {
+            File,
+            NuGet,
+            Submodule,
+        }
+        
+        public NeedType Type { get; set; }
+        
+        public string Key { get; set; }
+        
+        public string Version { get; set; }
+        
+        public string ReviewedBy { get; set; }
+        
+        public DateTime ReviewedAt { get; set; }
+    }
+    
+    public List<Need> ReviewedNeeds { get; set; }
 }
